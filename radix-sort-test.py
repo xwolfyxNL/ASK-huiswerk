4828eb1a056cb9a618f39f2585aee0e1191f0873
Stefan van den Eijkel <41stefan@gmail.com>
2019-01-30 22:29:10 +0100

    Begin voor positieve en negatieve nummers

--- radix-sort-test.py
+++ radix-sort-test.py
@@ -2,12 +2,25 @@ def my_sort(lst):
   # Waarde "RADIX" aanpassen voor grotere getallen, hoe groter hoe langer het sorteren duurt.
   # Waarde RADIX = 10000 kan getallen tot 9999
   RADIX = 10000
-  tmp , placement = -1, 1
+  tmp , placement, = -1, 1,
+  
+  # Splits de lijst in 1 met positieve en 1 met negatieve cijfers
+  lstpositive = []
+  lstnegative = []
+  for number in lst: 
+    if number > 0: 
+        lstpositive.append(number)
+    else: 
+        lstnegative.append(number)
+  
+  print ("Tussentijdse splitsing:")      
+  print (lstpositive)
+  print (lstnegative)
 
   while True:
     buckets = [list() for _ in range( RADIX )]
 
-    for i in lst:
+    for i in lstpositive:
       tmp = int((i / placement) % RADIX)
       buckets[tmp].append(i)
 
@@ -15,14 +28,18 @@ def my_sort(lst):
     for b in range( RADIX ):
       buck = buckets[b]
       for i in buck:
-        lst[a] = i
+        lstpositive[a] = i
         a += 1
 
     placement *= RADIX
+    
+    print ("Na de sortering:")
+    print (lstpositive)
+    print (lstnegative)
     return lst
 
 if __name__ == '__main__':
-    lst = [210, 7, 124, 9324, 32, 10]
+    lst = [210, -7, -124, 9324, 32, -10]
     print("Dit was de lijst voor de RADIX sortering:")
     print(lst)
     my_sort(lst)
